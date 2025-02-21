f1eb8d57b80018d10320e3b0948511252c234f3c
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-02-21 15:33:28 +0000

    Update gnomeExtensions.user-themes.nix

--- 4_configs/gsettings/gnomeExtensions.user-themes.nix
+++ 4_configs/gsettings/gnomeExtensions.user-themes.nix
@@ -1,1 +1,27 @@
+{ config, lib, pkgs, ... }:
 
+let
+  cfgpath = import /etc/nixos/cfgpath.nix;
+  machine = import /etc/machine.nix;
+  v = import (builtins.toPath "${cfgpath}/0_settings/${machine}-conf.nix");
+
+in
+{
+  home-manager.users.${v.username} = { lib, ... }: {
+    dconf = {
+      settings = {
+
+#-------------------------------
+# Insert DCONF here
+#-------------------------------
+
+        "org/gnome/shell/extensions/user-theme" = {
+          name = "Fluent-round-Dark";
+        };
+
+#-------------------------------
+
+      };
+    };
+  };
+}
