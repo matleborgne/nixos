79f95076ff49cf346d1863a6ec38c592a9e3dc01
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-02-21 15:46:58 +0000

    Create intelgpu.nix

---
+++ 1_hardware/intelgpu.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  #opengl renamed to graphics
+  hardware.graphics = {
+    enable = true;
+
+    driSupport = lib.mkDefault true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-media-sdk
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+
+    driSupport32Bit = lib.mkDefault true;
+    extraPackages32 = with pkgs; [
+      driversi686Linux.vaapiVdpau
+      driversi686Linux.libvdpau-va-gl
+    ];
+
+  };
+
+
+  environment.variables = {
+    LIBVA_DRIVER_NAME = lib.mkDefault "iHD";
+    VDPAU_DRIVER = lib.mkDefault "va_gl";
+  };
+
+}
