7dbd4e76419470c0aed7698c1befd0f0fc0a0fb0
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-02-21 15:23:53 +0000

    Update virt-manager.nix

--- 4_configs/gsettings/virt-manager.nix
+++ 4_configs/gsettings/virt-manager.nix
@@ -1,1 +1,40 @@
+{ config, lib, pkgs, ... }:
 
+let
+  cfgpath = import /etc/nixos/cfgpath.nix;
+  machine = import /etc/machine.nix;
+  v = import (builtins.toPath "${cfgpath}/0_settings/${machine}-conf.nix");
+
+in
+{
+  home-manager.users.${v.username} = { lib, ... }: {
+    dconf = {
+      settings = {
+
+#-------------------------------
+# Insert DCONF here
+#-------------------------------
+
+        "org/virt-manager/virt-manager" = {
+          enable-libguestfs-vm-inspection = true;
+          xmleditor-enabled = true;
+        };
+
+        "org/virt-manager/virt-manager/console" = {
+          resize-guest = lib.hm.gvariant.mkUint32 1;
+        };
+
+        "org/virt-manager/virt-manager/details" = {
+          show-toolbar = false;
+        };
+
+        "org/virt-manager/virt-manager/paths" = {
+          image-default = "~/.local/share/libvirt/images";
+        };
+
+#-------------------------------
+
+      };
+    };
+  };
+}
