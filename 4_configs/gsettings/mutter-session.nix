738fedc738cfdadbffbed776d1251875c3f20df4
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-02-21 15:23:00 +0000

    Update mutter-session.nix

--- 4_configs/gsettings/mutter-session.nix
+++ 4_configs/gsettings/mutter-session.nix
@@ -32,6 +32,30 @@ in
           sleep-inactive-ac-type = "nothing";
         };
 
+        # Raccourcis
+        "org/gnome/desktop/wm/keybindings" = {
+          move-to-workspace-left = ["<Alt><Shift>Left"];
+          move-to-workspace-right = ["<Alt><Shift>Right"];
+          switch-to-workspace-left = ["<Alt>Left"];
+          switch-to-workspace-right = ["<Alt>Right"];
+        };
+
+        # UI divers
+        "org/gnome/desktop/interface" = {
+          text-scaling-factor = lib.hm.gvariant.mkDouble 1.3;
+          clock-show-weekday = true;
+          font-name = "Ubuntu Regular 11";
+          document-font-name = "Ubuntu Regular 11";
+          monospace-font-name = "Ubuntu Mono Regular 12";
+          gtk-theme = "adw-gtk3";
+          icon-theme = "Papirus";
+        }; 
+
+        "org/gnome/desktop/wm/preferences" = {
+          button-layout = ":minimize,close";
+          titlebar-font = "Ubuntu Bold 11";
+        };
+
 #-------------------------------
 
       };
