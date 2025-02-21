1b4f0336e07e863f7a9c4a5182fd628cdeba015e
Mathieu Leborgne <101802281+matleborgne@users.noreply.github.com>
2025-02-21 15:30:40 +0000

    Update mutter.nix

--- 4_configs/gsettings/mutter.nix
+++ 4_configs/gsettings/mutter.nix
@@ -15,6 +15,14 @@ in
 # Insert DCONF here
 #-------------------------------
 
+        "org/gnome/settings-daemon/plugins/color" = {
+          night-light-enabled = true;
+          night-light-temperature = lib.hm.gvariant.mkUint32 4700;          
+          night-light-schedule-to = lib.hm.gvariant.mkDouble 3.9999;
+          night-light-schedule-from = lib.hm.gvariant.mkDouble 4.00;
+          night-light-schedule-automatic = false;
+        };
+
         "org/gnome/desktop/peripherals/touchpad" = {
           tap-to-click = true;
           speed = .25;
